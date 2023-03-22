696ff05d3116b348154e934a6de011c033768ae1
Tom <tom@glowtree.com>
2023-03-22 02:06:24 -0400

    wip on rez setup

---
+++ package.py
@@ -0,0 +1,17 @@
+
+name = 'gt_base'
+
+description = 'base classes and functions'
+
+authors = [
+  'Tom Sirdevan'
+]
+
+requires = [
+  'pybythec',
+  ''
+]
+
+private_build_requires = [
+  'pybythec'
+]
